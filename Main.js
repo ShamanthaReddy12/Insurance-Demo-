a86a49ae494999060322d1993d2f4c4138818ac5
shamantha <shamantha.kyasaram@gmail.com>
2019-07-28 17:21:40 +0530

    updated accordion, buttons and stories

--- Main.js
+++ Main.js
@@ -1,13 +1,61 @@
-   /**
-     * EFECTO PARA FLECHAS EN ACORDEON
-     */
-    
-    $(document).on('show','.accordion', function (e) {
-         //$('.accordion-heading i').toggleClass(' ');
-         $(e.target).prev('.accordion-heading').addClass('accordion-opened');
-    });
-    
-    $(document).on('hide','.accordion', function (e) {
-        $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
-        //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
-    });
+   
+    var accordions= document.getElementsByClassName("accordion");
+
+    for( var i=0; i < accordions.length; i++)
+    {
+        accordions[i].onclick = function() {
+
+            this.classList.toggle('is-open');
+
+            var content=this.nextElementSibling;
+
+        if(content.style.maxHeight)
+        {
+            content.style.maxHeight=null;
+
+           }
+           else
+           {
+            content.style.maxHeight=content.scrollHeight + "px" ;
+           }
+         }
+    }
+
+
+
+
+$(document).ready(function(){
+ $("#search").keyup(function(){
+ 
+ searchHighlight($(this).val());
+ });
+ 
+ });
+ 
+ function searchHighlight(searchText){
+ if(searchText){
+ 
+ var content =$("p").text();
+ var searchExp=new RegExp(searchText, "ig")
+ 
+ var matches= content.match(searchExp);
+ 
+  if(matches){
+  
+  $("p").html(content.replace(searchExp,function(match){
+  
+  return "<span class='highlight'>" + match + "</span>";
+  }));
+  
+  }
+   else{
+ $(".highlight").removeClass("highlight");
+ }
+ 
+ }
+ else{
+ $(".highlight").removeClass("highlight");
+ }
+ 
+ }
+
